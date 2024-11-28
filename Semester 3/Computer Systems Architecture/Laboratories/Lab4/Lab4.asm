a1cd52a421634feba995e1d6b9a7e10868b221b7
Mario Maxim <maximmarioandrei@gmail.com>
2024-11-28 21:21:30 +0200

    Add files via upload

---
+++ Semester 3/Computer Systems Architecture/Laboratories/Lab4/Lab4.asm
@@ -0,0 +1,40 @@
+;   A4. A string of bytes A is given. Construct string B such that each element from B represent the product of two consecutive elements from string A.
+;   If A = 2, 4, 5, 7 => B = 8, 20, 35
+
+bits 32 ; assembling for the 32 bits architectureS
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start 
+   
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    
+    s db 2, 4, 5, 7
+    len_sir equ $-s
+    b times len_sir-1 dw 0 
+   
+; our code starts here
+segment code use32 class=code
+    start:
+        mov ECX, len_sir-1
+        mov ESI, 0; pentru a parcurge sirul initial s
+        mov EDI, 0; pentru parcurgere sir rezultat b
+        
+        repeta:
+            mov AL, [s+ESI]
+            mov BL, [s+ESI+1]
+            mul BL; AX =s1*s2
+            mov [b+EDI], AX
+            inc ESI               
+            add EDI, 2                
+        loop repeta
+        
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
+         
---
+++ Semester 3/Computer Systems Architecture/Laboratories/Lab4/Lab4Ex2.asm
@@ -0,0 +1,39 @@
+;   B9. A string of doublewords S is given. Compute string D containing only low bytes from low words from each doubleword from string S.
+;   If S  = 51234678h, 1a2b3c4dh => D = 78h, 4dh
+
+bits 32 ; assembling for the 32 bits architectureS
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start 
+   
+; declare external functions needed by our program
+extern exit               ; tell nasm that exit exists even if we won't be defining it
+import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
+                          ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+
+    s dd 51234678h, 1a2b3c4dh
+    len_s equ ($-s)/4   ; 1 dd = 4 bytes
+    d times len_s db 0  
+
+   
+; our code starts here
+segment code use32 class=code
+    start:
+        mov ECX, len_s
+        mov ESI, 0; pentru a parcurge sirul initial s
+        mov EDI, 0; pentru parcurgere sir rezultat d
+        
+        repeta:
+            mov EAX, [s+ESI]
+            mov [d+EDI], AL
+            add ESI, 4
+            inc EDI
+        loop repeta
+            
+        ; exit(0)
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
+         
